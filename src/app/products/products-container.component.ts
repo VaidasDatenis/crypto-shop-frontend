418e646bf5c475ddcd57c710dfe22d47ffa49d43
Vaidas datenis <vaidas@Vaidass-MBP.lan>
2024-03-21 21:09:08 +0200

    display public itemss

--- src/app/products/products-container.component.ts
+++ src/app/products/products-container.component.ts
@@ -1,11 +1,29 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from "@angular/core";
 import { ItemComponent } from "./items/item.component";
+import { RequestsService } from "../services/requests.service";
+import { Product } from "../iterfaces/product.interface";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
   standalone: true,
   selector: 'products-container',
   imports: [ItemComponent],
   templateUrl: './products-container.component.html',
-  styleUrl: './products-container.component.scss'
+  styleUrl: './products-container.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductsContainerComponent {}
+export class ProductsContainerComponent implements OnInit {
+  requestService = inject(RequestsService);
+
+  listOfProducts = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    this.getListOfProducts();
+  }
+
+  getListOfProducts() {
+    this.requestService.getAllProducts().subscribe(
+      (products) => this.listOfProducts.set(products)
+    );
+  }
+}
